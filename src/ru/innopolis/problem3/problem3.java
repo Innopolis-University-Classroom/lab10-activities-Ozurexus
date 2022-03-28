91cb04538a060df84a6d4f25360b713fd2723abd
MaximPinyaguin <88331457+Ozurexus@users.noreply.github.com>
2022-03-28 13:57:08 +0300

    Update problem3.java

--- src/ru/innopolis/problem3/problem3.java
+++ src/ru/innopolis/problem3/problem3.java
@@ -1,2 +1,32 @@
 // your solution here
 //fix all the errors including package errors
+
+public class Factory {
+
+    enum Animals {Cat, Bear, Duck}
+
+    public Colour getAnimal(Animals animal) {
+        return switch (animal) {
+            case Cat -> new CAT(color);
+            case Bear -> new BEAR(color);
+            case Duck -> new DUCK(color);
+        };
+    }
+
+    void printColour(Colour colour) {
+        colour.PrintColor();
+    }
+
+    public static void main(String[] args) {
+        Factory factory = new Factory();
+        factory.printColour(factory.getAnimal(Animals.Cat));
+        factory.printColour(factory.getAnimal(Animals.Bear));
+        factory.printColour(factory.getAnimal(Animals.Duck));
+    }
+
+}
+public interface Colour {
+
+    void PrintColor();
+
+}
